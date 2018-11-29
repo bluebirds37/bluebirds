9db074565cee6f701271a0a23cfd115bfe5ea627
bluebirds37 <bluebirds37@163.com>
2018-11-29 12:51:26 +0800

    update

--- src/components/homeAside/HomeAside.vue
+++ src/components/homeAside/HomeAside.vue
@@ -1,45 +1,45 @@
 <template>
-    <el-row class="tac">
-        <el-col :span="24">
-            <el-menu
-                default-active="3"
-                class="el-menu-vertical-demo"
-                @open="handleOpen"
-                @close="handleClose"
-            >
-                <el-menu-item index="1">
-                    <i class="el-icon-search"></i>
-                    <span slot="title">
-                        <el-input
-                            placeholder="请输入"
-                            v-model="search"
-                            class="aside-input-search"
-                            style="width:100px"
-                            clearable
-                        ></el-input>
-                    </span>
-                </el-menu-item>
-                <el-menu-item index="2">
-                    <i class="el-icon-menu"></i>
-                    <span slot="title">
-                        <router-link to="/articleEdit">ArticleShow</router-link>
-                    </span>
-                </el-menu-item>
-                <el-menu-item index="3">
-                    <i class="el-icon-document"></i>
-                    <span slot="title">
-                        <router-link to="/articleShow">ArticleShow</router-link>
-                    </span>
-                </el-menu-item>
-                <el-menu-item index="4">
-                    <i class="el-icon-setting"></i>
-                    <span slot="title">
-                        <router-link to="/HomeArticleList">HomeArticleList</router-link>
-                    </span>
-                </el-menu-item>
-            </el-menu>
-        </el-col>
-    </el-row>
+  <el-row class="tac">
+    <el-col :span="24">
+      <el-menu
+        default-active="3"
+        class="el-menu-vertical-demo"
+        @open="handleOpen"
+        @close="handleClose"
+      >
+        <el-menu-item index="1">
+          <i class="el-icon-search"></i>
+          <span slot="title">
+            <el-input
+              placeholder="请输入"
+              v-model="search"
+              class="aside-input-search"
+              style="width:100px"
+              clearable
+            ></el-input>
+          </span>
+        </el-menu-item>
+        <el-menu-item index="2">
+          <i class="el-icon-menu"></i>
+          <span slot="title">
+            <router-link to="/articleEdit">ArticleShow</router-link>
+          </span>
+        </el-menu-item>
+        <el-menu-item index="3">
+          <i class="el-icon-document"></i>
+          <span slot="title">
+            <router-link to="/articleShow">ArticleShow</router-link>
+          </span>
+        </el-menu-item>
+        <el-menu-item index="4">
+          <i class="el-icon-setting"></i>
+          <span slot="title">
+            <router-link to="/HomeArticleList">HomeArticleList</router-link>
+          </span>
+        </el-menu-item>
+      </el-menu>
+    </el-col>
+  </el-row>
 </template>
 
 
